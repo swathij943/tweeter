751ec6aa3214e441f9e92e2e87dcd8ea431a06a2
swathij943 <swathij943@gmail.com>
2023-06-19 14:00:59 -0400

    Implemented AJAX GET request to fetch tweets from the database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,38 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-//driver code (tempotary). Eventuall it will get from the server
-// Fake data taken from initial-tweets.json
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
+//loop through database & filter each object to fill tweets info
 const renderTweets = function(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
   for (let tweet of tweets) {
     const $tweet = createTweetElement(tweet)
     $(".container").append($tweet)
@@ -96,8 +66,10 @@ const createTweetElement = function(tweetData) {
   return $tweet;
 }
 
-ajaxPostRequest = (url, method, data) => {
-  console.log('Starting ajax call')
+//AJAX POST request when posting a new tweet
+
+const ajaxPostRequest = (url, method, data) => {
+  console.log('Starting AJAX call')
   $.ajax({url, method, data})
   .then(response => {
     console.log('worked')
@@ -110,15 +82,25 @@ ajaxPostRequest = (url, method, data) => {
   });
 }
 
+const loadTweets = (url, method) => {
+  $.ajax({url, method})
+  .then(tweets => {
+    renderTweets(tweets)
+  })
+  .fail(err => {
+    console.log(err)
+  })
+}
 
-//shorthand of document ready menthod is $(() => {})
 
-$(document).ready(function() {
-  renderTweets(data);
+//shorthand of document ready menthod is $(() => {}) - $(document).ready(function() {})
 
+$(() => {
   $('#form').on('submit', function(event) {
     event.preventDefault();
     tweet = $(this).serialize();
     ajaxPostRequest('/tweets', 'POST', tweet);
   })
+
+  loadTweets("/tweets", "GET")
 });
