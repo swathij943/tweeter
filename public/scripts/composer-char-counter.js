18ef20744d6b23ebfe18810c0295aaec18881122
swathij943 <swathij943@gmail.com>
2023-06-19 17:32:14 -0400

    Implemented 2nd toggle button which scrolls back to top

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-  $("textBar").on('input', function() {
+  $('#downBtn').hide()
+
+  $("#textBar").on('input', function() {
     const maxCount = 140
     let inputLength = $(this).val().length
     $(this).nextAll('.counter').text(maxCount - inputLength)
@@ -8,6 +10,21 @@ $(document).ready(function() {
     } else {
       $(this).nextAll('.counter').css("color", "#545149");
     }
-
   });
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 450) {
+      $('#downBtn').fadeIn('100');
+      $('#navBtn').fadeOut('100');
+    } else {
+      $('#downBtn').fadeOut('100');
+      $('#navBtn').fadeIn('100');
+    }
+  });
+
+  $('#downBtn').on('click', function() {
+    $('body, html').animate({scrollTop: 0}, '300');
+    $('#form').slideDown();
+    $('textarea').focus();
+  })
 });
