d06c3c92279d99ab2fee91de772dd969dc9f0efd
swathij943 <swathij943@gmail.com>
2023-06-24 13:12:03 -0400

    Implemented Responsive Design & Refactoring

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,16 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-//loop through database & filter each object to fill tweets info
-
-const renderTweets = function(tweets) {
-  for (let tweet of tweets) {
-    const $tweet = createTweetElement(tweet)
-    $('#tweetSection').prepend($tweet)
-  }
-}
-
-//creating each tweet using info from the database
+//creating each tweet using info from the database(in-memory)
 
 const createTweetElement = function(tweetData) {
 
@@ -31,24 +22,23 @@ const createTweetElement = function(tweetData) {
   $('<p>')
     .text(tweetData["user"]["name"])
     .appendTo($headerDiv);
-  $header.append($headerDiv);
+    $header.append($headerDiv);
   $('<p>')
     .addClass('username')
     .text(tweetData["user"]["handle"])
-    .appendTo($header)
+    .appendTo($header);
 
   //append header to tweet article
 
   $tweet.append($header);
 
-  //create & append content div & main text
+  //create & append main content & main text
 
   let $content = $("<div>");
-  $('<p>')
+  $('<p>') 
     .text(tweetData["content"]["text"])
     .appendTo($content);
   $tweet.append($content);
-
   //create footer appended to the tweet article
 
   let $footer = $("<footer>");
@@ -59,32 +49,41 @@ const createTweetElement = function(tweetData) {
     let $footerDiv = $('<div>');
     $('<img>')
       .attr('src', '../images/flag.png')
-      .appendTo($footerDiv)
+      .appendTo($footerDiv);
     $('<img>')
       .attr('src', '../images/exchange.png')
-      .appendTo($footerDiv)
+      .appendTo($footerDiv);
     $('<img>')
       .attr('src', '../images/heart.png')
-      .appendTo($footerDiv)
+      .appendTo($footerDiv);
   
-    $footer.append($footerDiv)
+      $footer.append($footerDiv);
   $tweet.append($footer);
 
   return $tweet;
-}
+};
 
-//AJAX POST request when posting a new tweet
+//loops through the database and extract each object to fill the tweets information
+
+const renderTweets = function(tweets) {
+  for (let tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('#tweetSection').prepend($tweet);
+  }
+};
+
+//checks for error message and returns it for display
 
 const formValidation = (data) => {
   let errorMsg;
   if (!data) {
-    errorMsg = "Cannot submit empty tweet"
+    errorMsg = "Cannot submit empty tweet";
   }
   if (data.length > 140) {
-    errorMsg = "Cannot submit tweet over 140 characters"
+    errorMsg = "Cannot submit tweet over 140 characters";
   }
-  return errorMsg
-}
+  return errorMsg;
+};
 
 //submit an ajax request to add the tweet to the db asynchronously
 
@@ -94,32 +93,32 @@ const submitForm = (url, method, tweet) => {
       loadLastTweet('/tweets');
     })
     .fail(err => {
-      console.log(err)
-    })
-}
+      console.log(err);
+    });
+};
 //fetch or load all tweet async onto the page
 
 const loadTweets = url => {
   $.ajax({url, method: 'GET'})
     .then(tweets => {
-      renderTweets(tweets)
+      renderTweets(tweets);
     })
     .fail(err => {
-      console.log(err)
-    })
-}
-
+      console.log(err);
+    });
+};
 //load the newly submitted tweet onto the page
 
 const loadLastTweet = url => {
   $.ajax({url, method: 'GET'})
     .then(tweets => {
-      renderTweets([tweets[tweets.length - 1]])
+      renderTweets([tweets[tweets.length - 1]]);
     })
     .fail(err => {
-      console.log(err)
-    })
-}
+      console.log(err);
+    });
+};
+
 //shorthand of document ready menthod is $(() => {}) - $(document).ready(function() {})
 
 $(() => {
@@ -131,24 +130,29 @@ $(() => {
     let tweet = $('#form textarea').val();
 
 
-    const errorMsg = formValidation(tweet)
+    const errorMsg = formValidation(tweet);
 
     //if there is an error toggles down the error message
     if (errorMsg) {
-      $('.error').slideDown().text(errorMsg)
+      $('.error').slideDown().text(errorMsg);
 
       //clicking anywhere on the body will toggle the errorMsg back up 
       $('body').on('click', function() {
-        $('.error').slideUp()
-        $('textarea').focus()
-      })
+        $('.error').slideUp();
+        $('textarea').focus();
+      });
     } else {
       submitForm('/tweets', 'POST', tweet);
     }
-  })
+  });
+  //load all the tweets onto the page without refreshing 
+
   loadTweets("/tweets")
+
+  //To slide down the form to submit a new tweet
+
   $('#slideBtn').on('click', function() {
-    $('#form').slideToggle()
-    $('textarea').focus()
-  })
+    $('#form').slideToggle();
+    $('textarea').focus();
+  });
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,13 @@
 $$(document).ready(function() {
-  $('#downBtn').hide()
-  $('#form').hide()
+
+  //hides the form to submit the new tweet & back up button on load
+  $('#downBtn').hide();
+  $('#form').hide();
+
   $("#textBar").on('input', function() {
-    const maxCount = 140
-    let inputLength = $(this).val().length
-    $(this).nextAll('.counter').text(maxCount - inputLength)
+    const maxCount = 140;
+    let inputLength = $(this).val().length;
+    $(this).nextAll('.counter').text(maxCount - inputLength);
     if (inputLength > 140) {
       $(this).nextAll('.counter').css("color", "red");
     } else {
@@ -12,22 +15,24 @@ $$(document).ready(function() {
     }
   });
 
-//when the user scroll past 450px it displays the second toggle button
+//when the user scroll past 230px it displays the second toggle button
 $(window).scroll(function() {
-  console.log($(window).scrollTop())
-  if ($(window).scrollTop() > 450) {
-    $('#downBtn').fadeIn('50');
-    $('#navBtn').fadeOut('50');
-  } else {
-    $('#downBtn').fadeOut('50');
-    $('#navBtn').fadeIn('50');
-  }
-});
+  console.log($(window).scrollTop());
+    if ($(window).scrollTop() > 230) {
+      $('#downBtn').fadeIn('50');
+      $('#navBtn').fadeOut('50');
+    } else {
+      $('#downBtn').fadeOut('50');
+      $('#navBtn').fadeIn('50');
+    }
+  });
 //when clicking on the second toggle button it brings back to the top of the page, also focuses on the textarea
 $('#downBtn').on('click', function() {
   $('body, html').animate({scrollTop: 0}, '250', 'linear', () => {
     $('#form').slideDown();
     $('textarea').focus();
+    $('#downBtn').fadeOut('50');
+    $('#navBtn').fadeIn('50');
   });
-})
+});
 });
